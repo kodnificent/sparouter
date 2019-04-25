88f8ff1ca32c21f107d01ca79515a1cd63a0ddd0
sirvic-tech <nigerianinfohub@gmail.com>
2019-04-25 12:38:27 +0100

    made minor corrections to readme and api.md files

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ module.exports = (env, argv) => {
     }
     if(argv.mode === 'production') {
         config.output.filename = '[name].min.js';
-        //config.devtool = false;
         config.module.rules[0].use.options.plugins = config.module.rules[0].use.options.plugins.concat('transform-remove-console');
     }
     return config;
